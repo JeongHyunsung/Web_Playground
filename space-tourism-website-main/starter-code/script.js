ab19f2b764f0f7b7dc7e8dcabf0a0c6a1aef8ab9
JeongHyunsung <97275406+JeongHyunsung@users.noreply.github.com>
2024-03-03 11:10:30 +0000

    20240303

--- space-tourism-website-main/starter-code/script.js
+++ space-tourism-website-main/starter-code/script.js
@@ -5,24 +5,39 @@ const destination_d = document.getElementById("destination-d");
 const destination_p = document.getElementById("destination-p");
 const destination_l = document.getElementById("destination-l");
 const destination_t = document.getElementById("destination-t");
+const destination_i = document.getElementById("destination-i");
+import data from "./data.json";
 
 
-console.log(destination_button);
-
-destination_button.forEach((element, index, arr) =>{
+destination_button.forEach((element) =>{
+    let cur_dest = 0;
+    let nxt_dest = 0;
     console.log(element.classList);
     
     element.addEventListener("click", function(e){
-        if(element.classList.contains('moon-box')){cur_dest = 'Moon';}
-        else if(element.classList.contains("mars-box")){cur_dest = 'Mars'}
-        else if(element.classList.contains("europa-box")){cur_dest = 'Europa'}
-        else{cur_dest = 'Titan'}
+        const destination_button_cur = document.querySelector(".destination-subnav .mbox.cur-nav");
+        if(element.classList.contains('moon-box')){nxt_dest = 0;}
+        else if(element.classList.contains("mars-box")){nxt_dest = 1;}
+        else if(element.classList.contains("europa-box")){nxt_dest = 2;}
+        else{nxt_dest = 3;}
+        if(destination_button_cur.classList.contains('moon-box')){cur_dest = 0;}
+        else if(destination_button_cur.classList.contains('mars-box')){cur_dest = 1;}
+        else if(destination_button_cur.classList.contains('europa-box')){cur_dest = 2;}
+        else{cur_dest = 3;}
+        destination_button[cur_dest].classList.remove("cur-nav");
+        text_button[cur_dest].classList.remove("cur-nav");
+        status_button[cur_dest].classList.remove("visible");
+        destination_button[nxt_dest].classList.add("cur-nav");
+        text_button[nxt_dest].classList.add("cur-nav");
+        status_button[nxt_dest].classList.add("visible");
+        destination_d.innerText = "STR"
+        
+        console.log(data_obj);
 
 
-        console.log(cur_dest);
-        arr.forEach((elem) =>{
 
-        });
-    }
+        console.log(cur_dest, nxt_dest);
+        
+    });
 });
 
