6b16d05e82b6acb01b3dbb59d0573a9c35fad17e
JeongHyunsung <97275406+JeongHyunsung@users.noreply.github.com>
2024-03-03 14:50:19 +0000

    20240303

--- space-tourism-website-main/starter-code/script.js
+++ space-tourism-website-main/starter-code/script.js
@@ -6,13 +6,20 @@ const destination_p = document.getElementById("destination-p");
 const destination_l = document.getElementById("destination-l");
 const destination_t = document.getElementById("destination-t");
 const destination_i = document.getElementById("destination-i");
-import data from "./data.json";
 
+const crew_button = document.querySelectorAll(".crew-subnav div");
+const crew_m = document.getElementById("crew-m");
+const crew_n = document.getElementById("crew-n");
+const crew_p = document.getElementById("crew-p");
+const crew_i = document.getElementById("crew-i");
+
+
+
+import data from './data.js';
 
 destination_button.forEach((element) =>{
     let cur_dest = 0;
     let nxt_dest = 0;
-    console.log(element.classList);
     
     element.addEventListener("click", function(e){
         const destination_button_cur = document.querySelector(".destination-subnav .mbox.cur-nav");
@@ -30,14 +37,33 @@ destination_button.forEach((element) =>{
         destination_button[nxt_dest].classList.add("cur-nav");
         text_button[nxt_dest].classList.add("cur-nav");
         status_button[nxt_dest].classList.add("visible");
-        destination_d.innerText = "STR"
-        
-        console.log(data_obj);
-
-
-
-        console.log(cur_dest, nxt_dest);
-        
+        destination_d.innerText = data.destinations[nxt_dest].name.toUpperCase();
+        destination_p.innerText = data.destinations[nxt_dest].description;
+        destination_l.innerText = data.destinations[nxt_dest].distance.toUpperCase();
+        destination_t.innerText = data.destinations[nxt_dest].travel.toUpperCase();
+        destination_i.src = data.destinations[nxt_dest].images.webp;
     });
 });
 
+crew_button.forEach((element, index) =>{
+    let cur_crew = 0;
+    let nxt_crew = 0;
+    element.addEventListener("click", function(e){
+        const crew_button_cur = document.querySelector(".crew-subnav .cur-nav");
+        if(crew_button_cur.classList.contains("a")){cur_crew = 0;}
+        else if(crew_button_cur.classList.contains("b")){cur_crew = 1;}
+        else if(crew_button_cur.classList.contains("c")){cur_crew = 2;}
+        else{cur_crew = 3;}
+        nxt_crew = index;
+        crew_button[cur_crew].classList.remove("cur-nav");
+        crew_button[nxt_crew].classList.add("cur-nav");
+        crew_m.innerText = data.crew[nxt_crew].role.toUpperCase();
+        crew_n.innerText = data.crew[nxt_crew].name.toUpperCase();
+        crew_p.innerText = data.crew[nxt_crew].bio;
+        crew_i.src = data.crew[nxt_crew].images.webp;
+        crew_i.classList.remove('a', 'b', 'c', 'd');
+        crew_i.classList.add((nxt_crew == 1)? 'b':(nxt_crew == 2)? 'c':(nxt_crew == 3)? 'd':'a');
+        console.log(cur_crew, nxt_crew);
+    });
+
+});
