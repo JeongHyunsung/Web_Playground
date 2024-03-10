c96a70eed7621ae6807a662aba817505b965286d
JeongHyunsung <97275406+JeongHyunsung@users.noreply.github.com>
2024-03-10 05:03:42 +0000

    20240310

--- simple-react-app/src/App.js
+++ simple-react-app/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
       </header>
+      <body>
+        <h1> HELLO REACT </h1>
+        <h2> HELLO REACT </h2>
+        <h3> HELLO REACT </h3>
+      </body>
     </div>
   );
 }
