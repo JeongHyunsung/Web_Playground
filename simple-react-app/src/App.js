355c7924e0534fcdb9bdbddec088b7329b44f7b3
JeongHyunsung <97275406+JeongHyunsung@users.noreply.github.com>
2024-03-10 13:58:34 +0000

    20240310

--- simple-react-app/src/App.js
+++ simple-react-app/src/App.js
@@ -1,6 +1,19 @@
 import './App.css';
 
 
+function Head({cur_state}){
+  return(
+    <div className="header-ct">
+      <div className="header-desktop">
+        <Catalog state={0} cur_state={cur_state}/>
+        <Catalog state={1} cur_state={cur_state}/>
+        <Catalog state={2} cur_state={cur_state}/>
+        <Catalog state={3} cur_state={cur_state}/>
+      </div>
+    </div>
+  )
+}
+
 function Catalog({state, cur_state}){
   let circle;
   const data = ["YOUR INFO", "SELECT PLAN", "ADD-ONS", "SUMMARY"];
@@ -22,20 +35,15 @@ function Catalog({state, cur_state}){
   )
 }
 
-
-function Head({cur_state}){
+function Cont({cur_state}){
   return(
-    <div className="header-ct">
-      <div className="header-desktop">
-        <Catalog state={0} cur_state={cur_state}/>
-        <Catalog state={1} cur_state={cur_state}/>
-        <Catalog state={2} cur_state={cur_state}/>
-        <Catalog state={3} cur_state={cur_state}/>
-      </div>
+    <div className="content-ct">
+      <ContTitle cur_state={cur_state}/>
+      <ContMain cur_state={cur_state}/>
+      <ContControl cur_state={cur_state}/>
     </div>
   )
 }
-
 
 function ContTitle({cur_state}){
   const data = [["Personal info", "Please provide your name, email adress, and phone number."], 
@@ -51,9 +59,43 @@ function ContTitle({cur_state}){
 }
 
 function ContMain({cur_state}){
+  let content;
+  switch(cur_state){
+    case 0:
+      content = 
+        <div className="cont-1">
+          <form>
+            <FormSet tp="Name" df="Stephen King"/>
+            <FormSet tp="Email Address" df="stephenking@lorem.com"/>
+            <FormSet tp="Phone Number" df="+1 234 567 890"/>
+          </form>
+        </div>;
+      break;
+    default:
+      content =
+        <div className="cont-5">
+          
+        </div>
+  }
+
   return(
     <div className="cont-main">
+      {content}
+    </div>
+  )
+}
 
+function FormSet({tp, df}){
+  let default_value = df;
+  let error_message = "";
+  let blur_str = "this.placeholder=" + {df};
+  return(
+    <div>
+      <div className="form-upper">
+        <h4>{tp}</h4>
+        <p className="error-message">{error_message}</p>
+      </div>
+      <input className="form-lower" type="text" placeholder={df} onFocus={(e)=>{e.target.placeholder=""}} onBlur={(e)=>{e.target.placeholder=df}}/>
     </div>
   )
 }
@@ -62,16 +104,6 @@ function ContControl({cur_state}){
   return(
     <div className="cont-control">
 
-    </div>
-  )
-}
-
-function Cont({cur_state}){
-  return(
-    <div className="content-ct">
-      <ContTitle cur_state={cur_state}/>
-      <ContMain cur_state={cur_state}/>
-      <ContControl cur_state={cur_state}/>
     </div>
   )
 }
